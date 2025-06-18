565cd5205d5744077f7690feed9ea7e59cb10a52
Ruffy93 <rafael-neumann@web.de>
2025-06-18 15:29:20 +0200

    Test

--- VM_Setup.ps1
+++ VM_Setup.ps1
@@ -1,6 +1,9 @@
 # === 1. Remotedesktop aktivieren ===
 Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Terminal Server" -Name "fDenyTSConnections" -Value 0
-Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
+
+# Aktiviert spezifische Firewall-Regeln statt nach DisplayGroup zu suchen
+Enable-NetFirewallRule -Name "RemoteDesktop-UserMode-In-TCP"
+Enable-NetFirewallRule -Name "RemoteDesktop-UserMode-In-UDP"
 
 # === 2. PC-Namen ändern ===
 Rename-Computer -NewName "PS_Funktioniert" -Force
@@ -14,7 +17,7 @@ Set-LocalUser -Name "Administrator" -Password $adminPass
 
 # === 5. IP-Adresse ändern ===
 $ifIndex = (Get-NetAdapter | Where-Object {$_.Status -eq "Up"}).InterfaceIndex
-New-NetIPAddress -InterfaceIndex $ifIndex -IPAddress "192.168.178.222" -PrefixLength 24 -DefaultGateway "192.168.100.1"
+New-NetIPAddress -InterfaceIndex $ifIndex -IPAddress "192.168.100.222" -PrefixLength 24 -DefaultGateway "192.168.100.1"
 Set-DnsClientServerAddress -InterfaceIndex $ifIndex -ServerAddresses "8.8.8.8"
 
 # === 6. Hintergrundfarbe auf grün setzen ===
@@ -30,3 +33,7 @@ Start-Process "$bgPath\Bginfo64.exe" -ArgumentList "/timer:0 /silent /nolicpromp
 
 # === 8. Neustart der VM ===
 Restart-Computer -Force
+
+
+# Dies erstellt eine ausführbare Datei aus dem Skript
+#Invoke-ps2exe -inputFile "C:\Users\Ruffy\Documents\RuffysWorld\PowerShell\VM_Setup.ps1" -outputFile "C:\Users\Ruffy\Documents\RuffysWorld\PowerShell\VM_Setup.exe" -noConsole
